ff623fe49b23ccf9bbcc3873f35cee75ee003541
dattashreya <159568371+dattashreya@users.noreply.github.com>
2024-07-04 16:45:09 +0530

    Basic Queries in SQL

---
+++ queries.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE Student (
+  ID int PRIMARY KEY,
+  NAME varchar(20),
+  PH_NO varchar(10)
+);
+
+insert INTO Student VALUES
+  (1,'Shreya Datta','12345');
+insert INTO Student VALUES
+  (2,'Shree Dutta','22345');
+insert INTO Student VALUES
+  (3,'Shri Dutta','21345');
+SELECT * FROM Student;
